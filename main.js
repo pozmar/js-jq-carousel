e863f0f289d0a1c102f8cefbf665e1ed968ad4cb
Martina <martina.pozzerle@libero.it>
2020-11-03 16:34:09 +0100

    #img e dot

--- main.js
+++ main.js
@@ -20,3 +20,25 @@ $(".prev").click(function(){
     imgActiveEl.prev().addClass("active");
   }
 })
+//utilizzo le stesse funzioni per i pallini
+$(".next").click(function(){
+  var dotActiveEl = $("i.active");
+  dotActiveEl.removeClass("active");
+
+  if(dotActiveEl.hasClass("last")){
+    $(".first").addClass("active");
+  }else{
+    dotActiveEl.next().addClass("active");
+  }
+})
+
+$(".prev").click(function(){
+  var dotActiveEl = $("i.active");
+  dotActiveEl.removeClass("active");
+
+  if(dotActiveEl.hasClass("first")){
+    $(".last").addClass("active");
+  }else{
+    dotActiveEl.prev().addClass("active");
+  }
+})
